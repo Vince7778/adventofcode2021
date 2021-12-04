14848a6c53830a7ba4c8fa0610b94e241f85c193
Vince7778 <realvince7778@gmail.com>
2021-12-03 21:55:52 -0800

    Day 4

---
+++ 4a.hs
@@ -0,0 +1,68 @@
+import Data.List
+
+splitOn :: (Char -> Bool) -> String -> [String]
+splitOn p s = case dropWhile p s of
+    "" -> []
+    s' -> w : splitOn p s''
+        where (w, s'') = break p s'
+
+readBoard :: [String] -> [Int]
+readBoard strs =
+    let ss = take 5 strs
+        mps = concat $ map words ss
+    in map (read :: String -> Int) mps
+
+rowWins :: [Int] -> [Int] -> Bool
+rowWins board drawn
+    | length board < 5 = False
+    | otherwise =
+        let curRow = take 5 board
+        in (all (\x -> elem x drawn) curRow) || (rowWins (drop 5 board) drawn)
+
+everyNth :: Int -> [Int] -> [Int]
+everyNth _ [] = []
+everyNth n a@(x:xs) = x:(everyNth n $ drop n a)
+
+transpose' :: [Int] -> [Int]
+transpose' xs = concat $ map (\x -> everyNth 5 $ drop x xs) [0..4]
+
+-- Topright -> bottomleft diagonal only
+diagWins :: [Int] -> [Int] -> Bool
+diagWins board drawn =
+    let diag = everyNth 4 $ drop 4 board
+    in all (\x -> elem x drawn) diag
+
+boardWins :: [Int] -> [Int] -> Bool
+boardWins drawn board = 
+    let ck1 = (rowWins board drawn)-- || (diagWins board drawn)
+        tsp = transpose' board
+        ck2 = (rowWins tsp drawn)-- || (diagWins tsp drawn)
+    in ck1 || ck2
+
+readAllBoards :: [String] -> [[Int]]
+readAllBoards [] = []
+readAllBoards strs = (readBoard strs):(readAllBoards $ drop 6 strs)
+
+calcScore :: [Int] -> [Int] -> Int -> Int
+calcScore board rands ind =
+    let vals = take ind rands
+        rem = filter (\x -> notElem x vals) board
+    in (sum rem) * (last vals)
+
+getFirstScore :: [[Int]] -> [Int] -> Int -> Int
+getFirstScore boards rands ind =
+    let fin = filter (boardWins (take ind rands)) boards
+    in
+        if (length fin == 0) then
+            getFirstScore boards rands (ind+1)
+        else
+            calcScore (head fin) rands ind
+
+main :: IO()
+main = do
+    content <- getContents
+    let lns = lines content
+        rands = map (read :: String -> Int) $ splitOn (==',') $ head lns
+        allBoards = readAllBoards $ drop 2 lns
+    putStrLn $ show $ getFirstScore allBoards rands 1
+
---
+++ 4b.hs
@@ -0,0 +1,68 @@
+import Data.List
+
+splitOn :: (Char -> Bool) -> String -> [String]
+splitOn p s = case dropWhile p s of
+    "" -> []
+    s' -> w : splitOn p s''
+        where (w, s'') = break p s'
+
+readBoard :: [String] -> [Int]
+readBoard strs =
+    let ss = take 5 strs
+        mps = concat $ map words ss
+    in map (read :: String -> Int) mps
+
+rowWins :: [Int] -> [Int] -> Bool
+rowWins board drawn
+    | length board < 5 = False
+    | otherwise =
+        let curRow = take 5 board
+        in (all (\x -> elem x drawn) curRow) || (rowWins (drop 5 board) drawn)
+
+everyNth :: Int -> [Int] -> [Int]
+everyNth _ [] = []
+everyNth n a@(x:xs) = x:(everyNth n $ drop n a)
+
+transpose' :: [Int] -> [Int]
+transpose' xs = concat $ map (\x -> everyNth 5 $ drop x xs) [0..4]
+
+-- Topright -> bottomleft diagonal only
+diagWins :: [Int] -> [Int] -> Bool
+diagWins board drawn =
+    let diag = everyNth 4 $ drop 4 board
+    in all (\x -> elem x drawn) diag
+
+boardWins :: [Int] -> [Int] -> Bool
+boardWins drawn board = 
+    let ck1 = (rowWins board drawn)-- || (diagWins board drawn)
+        tsp = transpose' board
+        ck2 = (rowWins tsp drawn)-- || (diagWins tsp drawn)
+    in ck1 || ck2
+
+readAllBoards :: [String] -> [[Int]]
+readAllBoards [] = []
+readAllBoards strs = (readBoard strs):(readAllBoards $ drop 6 strs)
+
+calcScore :: [Int] -> [Int] -> Int -> Int
+calcScore board rands ind =
+    let vals = take ind rands
+        rem = filter (\x -> notElem x vals) board
+    in (sum rem) * (last vals)
+
+getFinalScore :: [[Int]] -> [Int] -> Int -> Int
+getFinalScore boards rands ind
+    | length boards == 0 = (-1)
+    | ind >= length rands = (-1)
+    | otherwise =
+        let fin = filter (boardWins $ take ind rands) boards
+            notFin = filter (\x -> not $ boardWins (take ind rands) x) boards
+            nxt = getFinalScore notFin rands (ind+1)
+        in if (nxt /= -1) then nxt else calcScore (head fin) rands ind
+
+main :: IO()
+main = do
+    content <- getContents
+    let lns = lines content
+        rands = map (read :: String -> Int) $ splitOn (==',') $ head lns
+        allBoards = readAllBoards $ drop 2 lns
+    putStrLn $ show $ getFinalScore allBoards rands 1
