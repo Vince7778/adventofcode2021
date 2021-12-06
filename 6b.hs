20782ac64ea56760aee8daeece4920d0fcc174cc
Vince7778 <realvince7778@gmail.com>
2021-12-05 21:40:52 -0800

    Day 6

---
+++ 6b.hs
@@ -0,0 +1,30 @@
+import Data.List
+import Data.Function (on)
+
+parseLine :: String -> [Int]
+parseLine s =
+    let groupComma = groupBy ((==) `on` (==',')) s
+        filtered = filter (/=",") groupComma
+    in map read filtered
+
+groupList :: [Int] -> [(Int, Int)]
+groupList xs = map (\all@(x:xs) -> (x, length all-1)) grouped
+    where grouped = group $ sort $ xs ++ [0..8]
+
+calcDay :: [(Int, Int)] -> [(Int, Int)]
+calcDay xs =
+    let subt = map (\(a,b) -> (a-1,b)) xs
+        elemn1 = snd $ head $ filter (\(a,b) -> a == (-1)) subt
+        added = map (\(a,b) -> if (a /= 6) then (a,b) else (a,b+elemn1)) subt
+    in tail (added ++ [(8, elemn1)])
+
+iterDay :: [(Int, Int)] -> [(Int, Int)]
+iterDay xs = (iterate calcDay xs) !! 256
+
+main :: IO()
+main = do
+    content <- getContents
+    let lns = lines content
+        parsed = parseLine $ head lns
+        ans = sum $ map snd $ iterDay $ groupList parsed
+    putStrLn $ show ans
